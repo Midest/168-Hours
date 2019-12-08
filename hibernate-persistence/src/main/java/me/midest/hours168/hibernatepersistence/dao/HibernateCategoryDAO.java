42c208ea0fdb79780dcbc6d160ec014ac5fd14c0
Dmitrii Molkov <midest.noreply@gmail.com>
2019-12-08 20:56:01 +0300

    Add check if category or its children referenced by time segments

--- hibernate-persistence/src/main/java/me/midest/hours168/hibernatepersistence/dao/HibernateCategoryDAO.java
+++ hibernate-persistence/src/main/java/me/midest/hours168/hibernatepersistence/dao/HibernateCategoryDAO.java
@@ -1,6 +1,7 @@
 package me.midest.hours168.hibernatepersistence.dao;
 
 import me.midest.hours168.core.dao.CategoryDAO;
+import me.midest.hours168.core.dao.exceptions.DeletionException;
 import me.midest.hours168.core.model.Category;
 import me.midest.hours168.hibernatepersistence.util.ParamBuilder;
 import org.springframework.stereotype.Repository;
@@ -39,11 +40,15 @@ public class HibernateCategoryDAO extends BaseCRUD<Category> implements Category
 
     @Override
     public Category deleteCategoryBranch( long id ) {
+        if( !canDeleteTree( getCategory( id )))
+            throw new DeletionException( id );
         return delete( id );
     }
 
     @Override
     public Category deleteCategorySingle( long id ) {
+        if( !canDelete( id ))
+            throw new DeletionException( id );
         Category category = getByID( id );
         if( category != null ){
             String query = "FROM categories WHERE parent_id=:pid";
@@ -82,6 +87,32 @@ public class HibernateCategoryDAO extends BaseCRUD<Category> implements Category
     @Override
     public Collection<Category> getCategories() {
         return getWithQuery( "FROM categories ORDER BY name ASC" );
+    }
+
+    /**
+     * Recursively check if we can delete category with its children.
+     * @param root category to check
+     * @return {@code true} if the whole tree can be deleted
+     */
+    private boolean canDeleteTree( Category root ){
+        if( canDelete( root.getId()))
+            for( Category ch : root.getChildren()){
+                if( !canDeleteTree( ch ))
+                    return false;
+            }
+        else return false;
+        return true;
+    }
+
+    /**
+     * Check if category can be deleted (i.e. has no segments reference it).
+     * @param id category id to check
+     * @return {@code true} if category can be deleted
+     */
+    private boolean canDelete( long id ){
+        String query = "FROM time_segments ts INNER JOIN ts.category c WHERE c.id=:id";
+        Map<String, Object> params = ParamBuilder.mapOf(1).put( "id", id ).get();
+        return getWithParamQuery( query, params, 1 ).isEmpty();
     }
 
 }
